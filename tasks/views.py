2fd189c723e1210f5e246c889f5107d721064fa2
Mohammad Jibran <yunisquadri99@gmail.com>
2025-04-15 04:44:53 +0530

    serialzers done

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.utils import timezone
+from .models import Task
+from .serializers import TaskSerialzier
 
-# Create your views here.
+
+class TaskListAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        tasks = Task.objects.all()
+
+
+        status = request.query_params.get("status")
+        assigned_user = request.query_params.get("assigned_user")
+        deadline_before = request.query_params.get("deadline_before")
+        deadline_after = request.query_params.get("deadlne_after")
+        is_pending = request.query_params.get("is_pending")
+
+        if status:
+            tasks = tasks.filter(status=status)
+
+        if assigned_user:
+            tasks = tasks.filter(assigned_users__id=assigned_user)
+
+        if deadline_before:
+            tasks = tasks.filter(deadline__lte=deadline_before)
+
+        if deadline_after:
+            tasks = tasks.filter(deadline__gte=deadline_after)
+
+        if is_pending == 'true':
+            now = timezone.now()
+            tasks = tasks.filter(status='pending').filter(deadline__isnull=True) | \
+                    tasks.filter(status='pending', deadline__gt=now)
+
+        serializer = TaskSerialzier(tasks.distinct(), many=True)
+        return Response(serializer.data)
